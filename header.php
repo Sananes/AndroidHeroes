98547f6deac5596fa2737cf7c25224954d6609a4
Aaron Sananes <aaron@sananes.co>
2013-03-24 15:43:08 +0000

    Update

--- header.php
+++ header.php
@@ -11,7 +11,7 @@
 <html <?php language_attributes(); ?>>
   <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>">
-    <title><?php the_title(); ?></title>
+    <title><?php the_title(); ?> - <?php wp_title(); ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
