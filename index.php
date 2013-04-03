d2f57aa9ebc49aab41e4d251ca2c91ba9e7d4939
Aaron Sananes <aaron@sananes.co>
2013-04-03 18:43:55 +0100

    Update

--- index.php
+++ index.php
@@ -13,281 +13,152 @@ get_header();
 
 
 ?>
-  <?php while ( have_posts() ) : the_post(); ?>
-
-      <header class="main-top">
-        <div class="container-fluid">
-          <div class="inner-container">
-              <h1 class="logo <?php if('android' == get_post_type()) { echo 'android'; } elseif('ux' == get_post_type()) { echo 'ui'; } elseif('magento' == get_post_type()) { echo 'magento'; } elseif('drupal' == get_post_type()) { echo 'drupal'; } elseif('joomla' == get_post_type()) { echo'joomla'; } else { echo ''; } ?>"><a href="<?php bloginfo('home'); ?>">
-                Android Heroes
-              </a>
-              </h1>
-              <?php if(get_field('sub_title')) : ?><p class="subleader"><?php the_field('sub_title'); ?></p><?php endif; ?>
-              <div class="clearfix"></div>
-              <div class="row-fluid">
-
-                <?php if('android' == get_post_type()) { ?>
-
-                <div class="intro slide-intro span6">
-                  <h2><?php the_field('intro_title'); ?></h2>
-                  <p><?php the_field('intro_description'); ?></p>
-                  <a href="#about" class="btn btn-success btn-large get-in-touch"><span>Get in touch</a>
-                </div> <!-- /intro -->
-
-               <div class="android-slider hidden-phone span9 offset6">
-                  <?php if(get_field('intro_image')) : ?><img src="<?php the_field('intro_image'); ?>" /><?php else: ?><img src="<?php theme_images(); ?>android-phone.png" /><?php endif; ?>
-
-                </div> <!-- /android-slider -->
-           
-                <?php } else { ?>      
-
-                <div class="intro slide-intro span7">
-                  <h2><?php the_field('intro_title'); ?></h2>
-                 
-                  <div class="tags">
-                     <?php while(has_sub_field('skillsets')): ?> 
-                    <span><?php the_sub_field('skillset'); ?></span>
-                     <?php endwhile; ?>
+<div class="container">
+<header class="header">
+<h1 class="logo">
+  <a href="<?php bloginfo('home'); ?>">CmSHeroes</a>
+</h1>
+<span class="slogan">
+  The network of proven developers
+  </span>
+   <div class="mini-contact offset5">
+                    <a href="mailto:info@cmshero.es">info@cmshero.es</a> <span class="phone">+1 09 08 8123</span>
                   </div>
-            
-                  <p><?php the_field('intro_description'); ?> </p>
-                  <a href="#about" class="btn btn-success btn-large get-in-touch"><span>Get in touch</a>
-                  <div class="mini-contact">
-                    <a href="mailto:info@cmshero.es">info@cmshero.es</a> <span class="phone">+1 098 998 001</span>
-                  </div>
-                </div> <!-- /intro -->
-
-                <div class="span5 profile">
-                  <img src="<?php the_field('profile_image'); ?>" width="350" height="350" class="profile-img" />
-                  <?php if(get_field('main_skill')) : ?><p><?php the_title(); ?> - <?php the_field('main_skill'); ?></p><?php else: ?><p><?php the_title(); ?> - Developer</p><?php endif; ?>
-                </div> <!-- /browser-slider -->
-                <?php } ?>
-              </div> <!-- /row-fluid -->
-          </div><!-- /inner-container -->
-        </div><!-- /container-fluid -->
-      </header> <!-- /main-top -->
-
-        <?php 
-         // or has_sub_field + the_sub_field
-        $projects = get_field('projects');
-
-        if($projects): ?>
-
-                <?php while(has_sub_field('projects')): ?>
-                <?php 
-                      $img_position = get_sub_field('image_position');
-                      $android = get_sub_field('android_color');
-                ?>
-          <section class="featured-work container-fluid <?php if($img_position == 'Left') { echo 'align-left'; } else { echo 'align-right'; } ?>">
-              <div class="row-fluid inner-container ">
-
-
-                 <?php if('android' == get_post_type()) { // show normal browser slider ?>
-                   <div class="feat-image-work android " >
-                 
-                  <img class="project-image" src="<?php theme_images(); ?>android-phone-<?php if($android == 'white') { echo 'white'; } else { echo 'black'; } ?>.png" />
-                   
-
-                  <div id="featured-carousel" class="featured-carousel carousel slide carousel-fade">
-                    
-                    <div class="carousel-inner">
-                  <?php while(has_sub_field('project_images')): ?> 
-                
-                  <!-- Carousel items -->      
-                  <div class="item">
-                    <img src="<?php the_sub_field('project_image'); ?>" />
-                  </div> <!-- /item -->
-                  <?php endwhile; ?>
-                  </div> <!-- /carousel-inner -->
-                  </div> <!-- /featured-carousel -->
-                   </div>  <!-- /feat-image-work -->
-                  <?php } else { // show android slider ?>
-                   <div class="feat-image-work browser " >
-                 
-                  <img class="project-image" src="<?php theme_images(); ?>default-browser-project.png" />
-                   
-
-                  <div id="featured-carousel" class="featured-carousel carousel slide carousel-fade">
-                    
-                    <div class="carousel-inner">
-                  <?php while(has_sub_field('project_images')): ?> 
-                
-                  <!-- Carousel items -->      
-                  <div class="item">
-                    <img src="<?php the_sub_field('project_image'); ?>" />
-                  </div> <!-- /item -->
-                  <?php endwhile; ?>
-                  </div> <!-- /carousel-inner -->
-                  </div> <!-- /featured-carousel -->
-                   </div>  <!-- /feat-image-work -->
-
-                  <?php } ?>
-                 
-                <div class="<?php if('magento' == get_post_type()) { echo 'span5'; } else { echo 'span7';} ?> details"><h2><?php the_sub_field('project_title'); ?></h2>
-                    <p class="lead"><?php the_sub_field('project_leader'); ?></p>
-
-                    <?php the_sub_field('project_details'); //Project details here ?>
-
-                    <?php if(get_sub_field('project_features')) : // Are there any features? ?>
-                    <ul class="list-features">
-                      <?php while(has_sub_field('project_features')) : // Are there any features? ?>
-                        <li><?php the_sub_field('project_feature'); ?></li>
-                      <?php endwhile; ?>
-                    </ul> <!-- / list-item -->
-                    <?php endif; ?>
-                    <a class="btn btn-large btn-secondary" href="<?php the_sub_field('project_button_link'); ?>"><?php the_sub_field('project_button_text'); ?></a>
-                </div><!-- /span6 -->
-          
-              </div> <!-- /row-fluid -->
-          </section> <!-- /featured-work container-fluid -->
-                 <?php endwhile; ?>
-        <?php endif; ?>
-      
-
+</header>
+<div class="clearfix"></div>
+<section id="platforms">
+ <div class="row-fluid">
+  <a href="<?php bloginfo('home'); ?>/magento/" class="platform-box span4">
    
-        <section class="portfolio container-fluid">
-          <div class="row-fluid inner-container">
-            <div class="span12 center-text">
-              <h2><?php echo $data['portfolio_title']; ?></h2>
-              <p class="lead"><?php echo $data['portfolio_subtitle']; ?></p>
-            </div><!-- /span12 -->
+      <img src="<?php theme_images(); ?>magento-heroes.png" />
+      <h2>Magento Hereoes</h2>
 
-              <!-- Here we output the portfolio images -->
-            <div class="portfolio-items">
-              <div class="row-fluid">
-                <?php while(has_sub_field('portfolio_images')): // Output Portfolio Images ?> 
-                <div class="portfolio-item span4">
-                  <img src="<?php if(get_sub_field('portfolio_image')) { ?><?php the_sub_field('portfolio_image'); ?><?php } else { echo theme_images() . 'portfolio-default.png'; } ?>" />                 
-                </div> <!-- /portfolio-item -->
-                <?php endwhile; ?>
-                
 
-            </div> <!-- /row-fluid -->
-          </div> <!-- /portfolio-items -->
-          </div> <!-- /row-fluid -->
-        </section> <!-- /portfolio container-fluid -->
-      
-        <section id="about" class="about-me">
-          <div class="container-fluid about-wrapper">
-              <div class="row-fluid inner-container">
-                <div class="span6">
-                  <h2><?php the_title(); ?></h2>
-                  <p><?php the_field('about_description'); ?></p>
-                  <div class="social inline"><?php the_field('social_media'); ?></div>
-                
-                  <div class="mini-contact">
-                    <a href="mailto:info@cmshero.es">info@cmshero.es</a> <span class="phone">+1 098 998 001</span>
-                  </div>
+</a>
+  <a href="<?php bloginfo('home'); ?>/ux/" class="platform-box span4">
 
-                </div> <!-- /span6 -->
+      <img src="<?php theme_images(); ?>ux-heroes.png" />
+      <h2>UX/UI Hereoes</h2>
 
-                 <div class="span6 profile">
-                  <img src="<?php the_field('profile_image'); ?>" width="350" height="350" class="profile-img" />
-                </div>
-
-                <div class="row-fluid">
-                  <div class="contact-form span12">
-                    <div class="form-inner">
-                      <?php include(TEMPLATEPATH . '/lib/includes/contact.form.php'); ?>
-                      </div> <!-- /form-inner -->
-            
-                    </div> <!-- /get-in-touch -->
-                </div>
-             
-              </div> <!-- /row-fluid -->
-        </div> <!-- /container-fluid -->
-      </section><!-- /about-me -->
-
-    
-       <section class="testimonials">
-          <div class="container-fluid">
-            <div class="inner-container">
-              <div class="row-fluid">
-                <div class="span12 center-text">
-                  <h2>Testimonials</h2>
-                </div> <!-- /span12 center-text -->
-                </div> <!-- /row-fluid -->
-                <div class="row-fluid">
-                <div class="testimonials-carousel carousel slide">
   
-                    <div class="carousel-inner">
-                  <?php $count = 3; ?>
-                
-                  <?php while(has_sub_field('testimonials')): ?> 
-               
-                    <?php if($count === 3) { echo '<div class="item row-fluid">'; } ?>
-                  <!-- Carousel items -->      
+</a>
+  <a href="<?php bloginfo('home'); ?>/frontend/" class="platform-box span4">
 
-                    <div class="span6"><blockquote class="clearfix">
-                    <?php the_sub_field('testimonial_content'); ?>
-                  </blockquote>
+      <img src="<?php theme_images(); ?>frontend-heroes.png" />
+      <h2>Frontend Hereoes</h2>
 
-                  <span class="client">
-                    <strong><?php the_sub_field('testimonial_client'); ?></strong><br />
-                    <a href="#"><?php the_sub_field('testimonial_link'); ?></a>
-                  </span> <!-- /client -->
-                  </div> <!-- /span6 -->
+  
+</a>
+</div> <!-- /row-fluid -->
+<div class="row-fluid">
+  <a href="<?php bloginfo('home'); ?>/ux/" class="platform-box span4">
 
-                  <?php if($count % 2 === 0) { echo '</div><div class="item row-fluid">'; } $count++;  ?> 
-                  
-                  <?php endwhile; ?>
-                  
-                  </div> <!-- /carousel-inner -->
-                  </div> <!-- /featured-carousel -->          
-              </div> <!-- /row-fluid -->
-            </div> <!-- /inner-container -->
-        </div> <!-- /container-fluid -->
-    
-    </section><!-- /testimonials -->
-
-        <section id="get-in-touch" class="contact">
+      <img src="<?php theme_images(); ?>drupal-heroes.png" />
+      <h2>Drupal Hereoes</h2>
 
 
-       
-                
-                <div class="company-information">
-                  <div class="inner">
-                  <h2>Get to know us</h2>
-                  <?php if($data['address']) { ?>
-                  <p class="address">
-                    <!-- 4th Floor, Star House
-                    15-17 Union Square
-                    San Francisco, California
-                    United States -->
-                    <?php echo $data['address']; ?>
-                  </p>
-                  <?php } ?>
-                  <?php if($data['phone_number']) { ?>
-                  <p class="phone-number"><strong>Call:</strong> <?php echo $data['phone_number']; ?></p>
-                  <?php } ?>
-                  <?php if($data['email_address']) { ?>
-                  <p class="email-address"><strong>Email:</strong> <a href="mailto:<?php $data['email_address']; ?>"><?php echo $data['email_address']; ?></a></p>
-                  <?php } ?>
-                  <?php if($data['twitter_link']) { ?>
-                  <p class="follow"><strong>Follow:</strong> <a href="<?php echo $data['twitter_link']; ?>"><?php echo $data['twitter_username']; ?></a></p>
-                  <?php } ?>
+</a>
+  <a href="<?php bloginfo('home'); ?>/wordpress/" class="platform-box span4">
 
-                  <?php if($data['linkedin']) { ?>
-                  <p class="linkedin"><strong>Company Profile:</strong> <a href="<?php echo $data['linkedin']; ?>">LinkedIn</a></p>
-                  <?php } ?>
+      <img src="<?php theme_images(); ?>wordpress-heroes.png" />
+      <h2>WordPress Hereoes</h2>
 
-                  <?php if($data['tax']) { ?>
-                  <p class="tax">
-                    <?php echo $data['tax']; ?>
-                    <!-- Evisua LTD
-                    Vat number: GB987472263 
-                    Company Number: 05997321
-                    -->
-                  </p>
-                  <?php } ?>
-                </div> <!-- /inner -->
-              </div> <!-- /company-information -->
-         
-           <div id="map_canvas" class="map">     </div> <!-- /map-canvas -->
-      </section><!-- /get-in-touch -->
+  
+</a>
+  <a href="<?php bloginfo('home'); ?>/android/" class="platform-box span4">
 
-       
-      <?php endwhile; // end of the loop. ?>
+      <img src="<?php theme_images(); ?>android-heroes.png" />
+      <h2>Android Hereoes</h2>
 
+  
+</a>
+</div> <!-- /row-fluid -->
+  </section>
+</div> <!-- /container -->
+</div> <!-- /wrapper -->
 
-<?php get_footer(); ?>
+<div id="footer">
+  <div class="container">
+  <p class="copyright">Copyright 2013. All Rights Reserved.</p>
+  <div class="follow">
+    <div class="twitter-follow"><a href="https://twitter.com/CmsHeroes" class="twitter-follow-button" data-show-count="false">Follow @CmsHeroes</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+  </div> 
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_GB/all.js#xfbml=1&appId=444979425585208";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+<div class="fb-like" data-href="https://www.facebook.com/CmSHeroes" data-send="false" data-layout="button_count" data-width="450" data-show-faces="false" data-font="lucida grande"></div>
+  <script src="//platform.linkedin.com/in.js" type="text/javascript"></script>
+<script type="IN/Share" data-url="http://www.linkedin.com/company/2750238?goback=%2Efcs_GLHD_cmsheroes_false_*2_*2_*2_*2_*2_*2_*2_*2_*2_*2_*2_*2&amp;trk=ncsrch_hits" data-counter="right"></script>
+  </div>
+</div> <!-- /container -->
+</div> <!-- /footer -->
+ <?php wp_footer(); ?>
+  <?php if($data['google_analytics']) { echo $data['google_analytics']; } ?>
+
+<!--
+                              ...                         
+                            ?7777I7                         
+                         =8OO$I8D8Z.                        
+                         8DDDI~:=+I                         
+                         8+D+=?ZIZ+                         
+                        7DO?+~~+~Z                          
+                        7D??=~I=7Z                          
+                        .?~=I+~=Z                           
+                      .=I=:~+$,,.                           
+                    .+I7+==+?$+,.                           
+                 .=?=??7+?=???++~7.                         
+                .II+~I+O7I$,=+~.77$,                        
+                $Z$?=::~~~7+  .,~Z+:.                       
+              .+ZO7$Z$II?=?$.....O??:      =+I              
+             :I?$7+O$8$:$$7I,.,,,+OI?    ,+??++             
+           .+O7?::I8Z?+:8+$I::~~~~ZIO=~  ~7?=+,             
+           ?Z$?=~7$8I?I7$7D~,,,::=OO?II:?I7I=.              
+          :?~~=?$ZOZ?I?O7Z::..~~~=7O$O~~=?7.                
+           ~I====?Z$$I$ZZ~::..  ,=7Z+==777                  
+            .?I=~~+I++7ZO,   ..::=+I=77$+.                  
+               ?I+=~=I?77,+Z$87OI$$ZI$+                     
+                .,ZI~++?I8OM8+ZDND$7D            ..         
+                  ??Z?+=+?=DOZZZO8OIZ:.                     
+                  =+IOI+++=O88ODZOND88=                     
+                  $?7I8OOIDZDZDDZ8N88OI                     
+                  I?:=O~DO8Z8$8DODDDDDO=                    
+                  7?I+8=DO8$8$DNOD8DNOD7.                   
+                  ?+?+O78N8ZOZZNZDDNN8NZ.                   
+                  I$?:DOZDZD$DZNODMMMNO$~                   
+                  ?$8NO8Z8$D$DZNMMNMNM8ZI.                  
+                  ,.  .O$OZ8$DDDMMMMNN8Z7.                  
+                      ,8$ZD$Z8NNINNM8NDOZ.                  
+                     .?Z8ZD$D8D: ,MMDM8ZO=                  
+                     :DODZNZ8D$  .7M8MNODI                  
+                    .IZ8NODZ8O   .:NDNM8NZ.                 
+                    .8$D$8ODO,   .:IDNMOD7:                 
+                   =$OOZONZ8,     .$N8NONZ:.                
+                  .OZDNDNZO,      .8MNNDZO,:                
+                  :DZNODZI.      .:DDNODZI.                 
+                 .O8ZO8DZ,      .:7D8NODZ:                  
+                 IDONDND:       .~$8ON88Z:                  
+                .ODNDD$+        .:8DNNND7.                  
+               .?888ZD8.         ~ZDO8ND+.                  
+               :DOOD8$:.         $8DDDNNZ.                  
+              .8DO88D$:          ZDDDDDD7                   
+              .ODNNDN?.          :DNNNNN,                   
+              .ODMNNZ,           ,DNNNN8:                   
+             .$DDNMN?.           ZDNDDDD:..                 
+             .$OMMNN~            ~DD78NNZ$ZO$:.             
+             .7ONN88$:,          IO88NDDOZ8ZZOZ.            
+             .$D8$ZZZ7,          ?DZ$$NDZZZZ$$O.            
+              7DD8ZZOD?.             ...,=77:..:~ZZ         
+               77OD88ZO.                                    
+                .ZD88I.. 
+
+                    Like a glooove!        
+
+-->
+</body>
+</html>
